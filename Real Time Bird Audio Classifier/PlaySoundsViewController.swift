13ebdeb9cf10fda3f550bfd7449f25182b67dc1e
Kavilan Nair <kavilann@gmail.com>
2018-08-06 13:46:47 +0200

    Added Playback and different filters, as it stands its based off udacity

--- Real Time Bird Audio Classifier/PlaySoundsViewController.swift
+++ Real Time Bird Audio Classifier/PlaySoundsViewController.swift
@@ -11,8 +11,6 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    var recordedAudioURL: URL!
-    
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var chipMunkButton: UIButton!
     @IBOutlet weak var rabbitButton: UIButton!
@@ -21,18 +19,51 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+        
+    }
+    
+    
     @IBAction func playSoundForButton(_ sender: UIButton){
-        print("play sound button pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound()
+        }
+        
+        configureUI(.playing)
     }
     
     @IBAction func stopButtonPressed(_ sender: AnyObject){
-        print("Stop audio button pressed")
+        stopAudio()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
 
     override func didReceiveMemoryWarning() {
