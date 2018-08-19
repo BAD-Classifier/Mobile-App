adc7af985f005a0bee66d517709444b8d7efb4c5
Kavilan Nair <kavilann@gmail.com>
2018-08-19 11:43:45 +0200

    implemented ability to sign out

---
+++ Real Time Bird Audio Classifier/ProfileViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ProfileViewController.swift
+//  Real Time Bird Audio Classifier
+//
+//  Created by Kavilan Nair on 2018/08/19.
+//  Copyright © 2018 Lab Proj. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+    @IBAction func signOut(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            performSegue(withIdentifier: "signOutSegue", sender: nil)
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let user = Auth.auth().currentUser
+        if let user = user {
+            // The user's ID, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server,
+            // if you have one. Use getTokenWithCompletion:completion: instead.
+            let uid = user.uid
+            let email = user.email
+            let photoURL = user.photoURL
+            usernameLabel?.text = uid
+            emailLabel?.text = email
+            // ...
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Real Time Bird Audio Classifier/RecordSoundsViewController.swift
+++ Real Time Bird Audio Classifier/RecordSoundsViewController.swift
@@ -31,17 +31,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let user = Auth.auth().currentUser
-        if let user = user {
-            // The user's ID, unique to the Firebase project.
-            // Do NOT use this value to authenticate with your backend server,
-            // if you have one. Use getTokenWithCompletion:completion: instead.
-            let uid = user.uid
-            let email = user.email
-            let photoURL = user.photoURL
-            // ...
-        }
-        print("\(user?.uid!)")
+        
         
         stopRecordingButton.isEnabled = false
         classificationLabel?.text = " "
